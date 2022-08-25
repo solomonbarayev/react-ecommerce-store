c9c098891eb13ee08c1e38a2b42f0b7a7879f7a2
solomonbarayev <sbarayev2@gmail.com>
2022-08-25 16:24:00 +0300

    add checkout access only to logged in users

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext();
+  const { myUser, loginWithRedirect } = useUserContext();
 
   return (
     <Wrapper>
@@ -24,9 +25,16 @@ const CartTotals = () => {
             <span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        <Link to="/checkout" className="btn">
-          Proceed to checkout
-        </Link>
+        {myUser && (
+          <Link to="/checkout" className="btn">
+            Proceed to checkout
+          </Link>
+        )}
+        {!myUser && (
+          <Link to={loginWithRedirect} className="btn">
+            Login to checkout
+          </Link>
+        )}
       </div>
     </Wrapper>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,7 @@ import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
   const { isSideBarOpen, closeSideBar } = useProductsContext();
+  const { myUser } = useUserContext();
   return (
     <SidebarContainer>
       <aside className={`sidebar ${isSideBarOpen && 'show-sidebar'}`}>
@@ -27,11 +28,13 @@ const Sidebar = () => {
               </Link>
             </li>
           ))}
-          <li>
-            <Link to="/checkout" onClick={closeSideBar}>
-              checkout
-            </Link>
-          </li>
+          {myUser && (
+            <li>
+              <Link to="/checkout" onClick={closeSideBar}>
+                checkout
+              </Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </aside>
